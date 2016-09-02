477598604e329238d93d03175264c475f84bb21b
Gustavo Salvi <tatosalvi@gmail.com>
2016-09-02 19:57:04 -0300

    subidos comentarios punto tp2ej1

--- tp02/ej01/Punto.cs
+++ tp02/ej01/Punto.cs
@@ -6,26 +6,34 @@ using System.Threading.Tasks;
 
 namespace ejercicio1
 {
+    // clase punto
     class Punto
     {
+        // coordenadas x e y del punto
         private double iX, iY;
 
+        // constructor: Punto(coordenada X, coordenada Y)
+        // ambos valores deben ser de tipo double
         public Punto(double pX, double pY)
         {
             iX = pX;
             iY = pY;
         }
 
+        // propiedad que devuelve un double equivalente a la coordenada X del punto
         public double X
         {
             get { return iX; }
         }
 
+        // propiedad que devuelve un double equivalente a la coordenada Y del punto
         public double Y
         {
             get { return iY; }
         }
 
+        // devuelve un double equivalente a la distancia entre el Punto
+        // y otro Punto pPunto que se pasa como paramentro.
         public double CalcularDistanciaDesde(Punto pPunto)
         {
             return (Math.Sqrt(Math.Pow((iX - pPunto.X), 2) + Math.Pow((iY - pPunto.Y), 2)));
--- tp02/ej01/Triángulo.cs
+++ tp02/ej01/Triángulo.cs
@@ -6,10 +6,14 @@ using System.Threading.Tasks;
 
 namespace ejercicio1
 {
+    // clase triangulo
     class Triángulo
     {
-        Punto iPunto1, iPunto2, iPunto3;
+        // vertices, su orden es indistinto
+        private Punto iPunto1, iPunto2, iPunto3;
 
+        // constructor: Triangulo(vertice 1, vertice 2, vertice 3)
+        // los vertices tienen que ser objetos de clase Punto
         public Triángulo(Punto pPunto1, Punto pPunto2, Punto pPunto3)
         {
             iPunto1 = pPunto1;
@@ -17,36 +21,43 @@ namespace ejercicio1
             iPunto3 = pPunto3;
         }
 
+        // devuelve el objeto de clase Punto que representa las coordenadas del vertice 1
         public Punto Punto1
         {
             get { return iPunto1; }
         }
 
+        // devuelve el objeto de clase Punto que representa las coordenadas del vertice 2
         public Punto Punto2
         {
             get { return iPunto2; }
         }
 
+        // devuelve el objeto de clase Punto que representa las coordenadas del vertice 3
         public Punto Punto3
         {
             get { return iPunto3; }
         }
 
+        // l1, l2 y l3 son accesores que representan los lados del triangulo, estan para
+        // mayor claridad en el codigo.
+        // l1: lado que va desde el punto 1 al punto 2
+        // l2: lado que va desde el punto 1 al punto 3
+        // l3: lado que va desde el punto 2 al punto 3
         private double l1
         {
             get { return iPunto1.CalcularDistanciaDesde(iPunto2); }
         }
-
         private double l2
         {
             get { return iPunto1.CalcularDistanciaDesde(iPunto3); }
         }
-
         private double l3
         {
             get { return iPunto2.CalcularDistanciaDesde(iPunto3); }
         }
 
+        // devuelve un double equivalente al area del triangulo, usa la Formula de Heron
         public double Área
         {
             get
@@ -56,6 +67,7 @@ namespace ejercicio1
             }
         }
 
+        // devuelve un double equivalente a la suma de los tres lados
         public double Perímetro
         {
             get { return (this.l1 + this.l2 + this.l3); }
