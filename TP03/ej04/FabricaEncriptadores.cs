18eb8fc917606796f301519c542baed9ed0c5a6a
Gustavo Salvi <tatosalvi@gmail.com>
2016-10-07 20:24:59 -0300

    me voy por la fiesta gay

---
+++ TP03/ej04/FabricaEncriptadores.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej04
+{
+    class FabricaEncriptadores
+    {
+        private static readonly Lazy<FabricaEncriptadores> cinstancia = new Lazy<FabricaEncriptadores>(() => new FabricaEncriptadores());
+
+        private FabricaEncriptadores() { }
+
+        public static FabricaEncriptadores Instancia
+        {
+            get { return cinstancia.Value; }
+        }
+
+
+    }
+}
---
+++ TP03/ej04/IEncriptador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej04
+{
+    interface IEncriptador
+    {
+        public string Encriptar(string pCadena);
+        public string Desencriptar(string pCadena);
+    }
+}
