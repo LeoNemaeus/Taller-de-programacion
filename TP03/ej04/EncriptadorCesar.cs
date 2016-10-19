d87c4fa6fabba1f65a4410f9a4d0ea0849164934
Gustavo Salvi <tatosalvi@gmail.com>
2016-10-19 19:02:49 -0300

    subir y mergear

--- TP03/ej04/EncriptadorCesar.cs
+++ TP03/ej04/EncriptadorCesar.cs
@@ -9,19 +9,79 @@ namespace ej04
     class EncriptadorCesar : Encriptador
     {
         private int iDesplazamiento;
-        
-        public EncriptadorCesar(int pDesplazamiento)
+        // alfabeto, se necesita para el algoritmo del encriptador
+        private static string abc = "abcdefghijklmñnopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890_-+,#$%&/()=¿?¡!|,.;:{}[]";
+        // metodo interno usado por el encriptador
+        private static int getPosABC(char caracter)
         {
+            for ( int i = 0; i<abc.Length ; i++)
+            {
+                if (caracter == abc[i])
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
 
+        public EncriptadorCesar(int pDesplazamiento) : base("César")
+        {
         }
 
         public override string Encriptar(string pCadena)
         {
-            return pCadena; //TODO: buscar en wikipedia encriptacion cesar
+
+            int desplazamiento = iDesplazamiento;
+            String cifrado = "";
+            if (desplazamiento > 0 && desplazamiento < pCadena.Length)
+            {
+                for (int i = 0; i < pCadena.Length; i++)
+                {
+                    int posCaracter = getPosABC(pCadena[i]);
+                    if (posCaracter != -1) //el caracter existe en la variable abc
+                    {
+                        int pos = posCaracter + desplazamiento;
+                        while (pos >= abc.Length)
+                        {
+                            pos = pos - abc.Length;
+                        }
+                        //concatena al mensaje cifrado
+                        cifrado += abc[pos];
+                    }
+                    else//si no existe el caracter no se cifra
+                    {
+                        cifrado += pCadena[i];
+                    }
+                }
+            }
+            return cifrado;
         }
+
+
         public override string Desencriptar(string pCadena)
         {
-            return pCadena;
+            String cifrado = "";
+            if (iDesplazamiento > 0 && iDesplazamiento < abc.Length)
+            {
+                for (int i = 0; i < pCadena.Length; i++)
+                {
+                    int posCaracter = getPosABC(pCadena[i]);
+                    if (posCaracter != -1) //el caracter existe en la variable abc
+                    {
+                        int pos = posCaracter - iDesplazamiento;
+                        while (pos < 0)
+                        {
+                            pos = pos + abc.Length;
+                        }
+                        cifrado += abc[pos];
+                    }
+                    else
+                    {
+                        cifrado += pCadena[i];
+                    }
+                }
+            }
+            return cifrado;
         }
     }
 }
