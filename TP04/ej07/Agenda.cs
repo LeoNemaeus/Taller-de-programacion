ab4c953eb5b848f536b0bec0948194ec592e0d37
Sandro Pastorini <sandropastorini@gmail.com>
2016-11-09 22:17:34 -0300

    Agenda inception
    Clases Agenda, Calendario, Evento fueron agregadas. Agenda tiene sus
    métodos CRUD

---
+++ TP04/ej07/Agenda.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//TODO propiedades, comentar Agenda
+//DOING definir métodos de Agenda
+namespace ej07
+{
+    class Agenda
+    {
+        private IDictionary<String, Calendario> iCalendarios = new SortedDictionary<String, Calendario>();
+
+        public Agenda() { }
+
+        public void agregarCalendario(string pNombre)
+        {
+            Calendario mCalendario = new Calendario(pNombre);
+            iCalendarios.Add(pNombre, mCalendario);
+        }
+
+        public void actualizarCalendario(Calendario pCalendario)
+        {
+            iCalendarios[pCalendario.Nombre] = pCalendario;
+        }
+
+        public void eliminarCalendario(string pNombreCalendario)
+        {
+            iCalendarios.Remove(pNombreCalendario);
+        }
+    }
+}
