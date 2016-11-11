ddc8d6fe736d88a739cf64769542a5daa06beb72
Sandro Pastorini <sandropastorini@gmail.com>
2016-11-11 19:37:38 -0300

    Implementación del criterio Duración.
    Aún no fue probado

---
+++ TP04/ej07/patron filter/CriterioDuracion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//TODO excepciones, testear
+//DOING implementar, documentar, 
+namespace ej07.patrón_filter
+{
+    /// <summary>
+    /// Permite determinar si un Evento cumple con una duración determinada.
+    /// </summary>
+    public class CriterioDuracion : ICriterio
+    {
+        private TimeSpan iDuracion;
+
+        CriterioDuracion(TimeSpan pDuracion)
+        {
+            this.iDuracion = pDuracion;
+        }
+
+        /// <summary>
+        /// Verifica que un evento tenga la duración definida por el filtro.
+        /// </summary>
+        /// <param name="pEvento"></param>
+        /// <returns></returns>
+        public bool cumpleCriterio(Evento pEvento)
+        {
+            return (pEvento.Duracion == iDuracion);
+        }
+    }
+}
---
+++ TP04/ej07/patron filter/ICriterio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//TODO documentar
+namespace ej07.patrón_filter
+{
+    public interface ICriterio
+    {
+        /// <summary>
+        /// Permite determinar si un Evento cumple con un criterio.
+        /// </summary>
+        /// <param name="pEvento">Un Evento</param>
+        /// <returns></returns>
+        bool cumpleCriterio(Evento pEvento);
+    }
+}
