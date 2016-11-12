9326cf734fd81dd087d9dcb10c2d0e1bcaabe5c8
Sandro Pastorini <sandropastorini@gmail.com>
2016-11-11 21:37:03 -0300

    Implementación y prueba CriterioOr
    Implementación y validación mediante tests unitarios del criterio
    compuesto CriterioOr.

---
+++ TP04/ej07/patron filter/CriterioOr.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//TODO  excepciones
+//UNDONE implementar, documentar, testear
+namespace ej07.patrón_filter
+{
+    //Criterio compuesto OR
+    public class CriterioOr : ICriterio
+    {
+        private ICriterio iUnCriterio, iOtroCriterio;
+        public CriterioOr(ICriterio pUnCriterio, ICriterio pOtroCriterio)
+        {
+            this.iUnCriterio = pUnCriterio;
+            this.iOtroCriterio = pOtroCriterio;
+        }
+
+        /// <summary>
+        /// Valida que un Evento cumpla al menos uno de dos Criterios dados.
+        /// </summary>
+        /// <param name="pEvento"></param>
+        /// <returns></returns>
+        public bool cumpleCriterio(Evento pEvento)
+        {
+            return (this.iUnCriterio.cumpleCriterio(pEvento) ||
+                (this.iOtroCriterio.cumpleCriterio(pEvento)));
+        }
+    }
+}
---
+++ TP04/ej07Tests/patron filter/CriterioOrTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ej07.patrón_filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej07.patrón_filter.Tests
+{
+    [TestClass()]
+    public class CriterioOrTests
+    {
+        [TestMethod()]
+        public void CriterioOrTest()
+        {
+            ICriterio unCriterio = new CriterioFecha(new DateTime(2016, 11, 12));
+            ICriterio otroCriterio = new CriterioNombre("Asd");
+            ICriterio criterioOr = new CriterioOr(unCriterio, otroCriterio);
+
+            Assert.IsNotNull(criterioOr);
+        }
+
+        [TestMethod()]
+        public void cumpleCriterioTest()
+        {
+            ICriterio unCriterio = new CriterioFecha(new DateTime(2016, 11, 12));
+            ICriterio otroCriterio = new CriterioNombre("Asd");
+            ICriterio criterioOr = new CriterioOr(unCriterio, otroCriterio);
+
+            Evento unEvento = new Evento("Asd", new DateTime(2016, 11, 12), TimeSpan.FromHours(1), Frecuencia.Unico);
+
+            Assert.IsTrue(criterioOr.cumpleCriterio(unEvento));
+        }
+    }
+}
