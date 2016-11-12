d5acca19d767ea996a16b514097a6275ef3563d0
Sandro Pastorini <sandropastorini@gmail.com>
2016-11-11 21:27:13 -0300

    Implementación Criterio Fecha
    Implementación y validación mediante tests unitarios de Criterio Fecha.

--- TP04/ej07/patron filter/CriterioFecha.cs
+++ TP04/ej07/patron filter/CriterioFecha.cs
@@ -4,10 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//TODO implementar, documentar, excepciones, testear
+//TODO excepciones, testear
+//DOING implementar, documentar
 namespace ej07.patrón_filter
 {
-    class CriterioFecha
+    /// <summary>
+    /// Permite determinar si un Evento se encuentra en un rango de fechas.
+    /// Si se provee una única fecha, se busca todos los eventos de esa fecha.
+    /// </summary>
+    public class CriterioFecha : ICriterio
     {
+        DateTime iFechaInicio, iFechaFin;
+
+        public CriterioFecha(DateTime pFechaInicio, DateTime pFechaFin)
+        {
+            this.iFechaInicio = pFechaInicio;
+            this.iFechaFin = pFechaFin;
+        }
+
+        public CriterioFecha(DateTime pFecha)
+        {
+            this.iFechaInicio = pFecha;
+        }
+
+        /// <summary>
+        /// Verifica que un Evento se encuentre en un rango de fechas.
+        /// </summary>
+        /// <param name="pEvento"></param>
+        /// <returns></returns>
+        public bool cumpleCriterio(Evento pEvento)
+        {
+            if (this.iFechaFin != null)
+            {
+                //El Evento comienza después y termina antes del límite inferior del rango
+                return ((this.iFechaInicio.CompareTo(pEvento.Comienzo) <= 0)
+                    &&
+                    (this.iFechaFin.CompareTo(pEvento.Comienzo) >= 0));
+
+            } else
+            {
+                return (pEvento.Comienzo == this.iFechaInicio);
+            }
+
+        }
     }
 }
---
+++ TP04/ej07Tests/patron filter/CriterioFechaTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ej07.patrón_filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej07.patrón_filter.Tests
+{
+    [TestClass()]
+    public class CriterioFechaTests
+    {
+        [TestMethod()]
+        public void CriterioFechaTest()
+        {
+            ICriterio unCriterio = new CriterioFecha(new DateTime(2016, 11, 12), new DateTime(2016, 12, 12));
+            Assert.IsNotNull(unCriterio);
+        }
+
+        [TestMethod()]
+        public void CriterioFechaTest1()
+        {
+            ICriterio unCriterio = new CriterioFecha(new DateTime(2016, 11, 12));
+            Assert.IsNotNull(unCriterio);
+        }
+
+        [TestMethod()]
+        public void cumpleCriterioTest()
+        {
+            Evento unEvento = new Evento("", new DateTime(2016, 11, 24), TimeSpan.FromHours(1), Frecuencia.Unico);
+            ICriterio unCriterio = new CriterioFecha(new DateTime(2016, 11, 12), new DateTime(2016, 12, 12));
+            Assert.IsTrue(unCriterio.cumpleCriterio(unEvento));
+        }
+    }
+}
